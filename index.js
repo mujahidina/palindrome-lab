f6eb21c357c594a74e8b44f9f99882f15a60f51e
mujahidina <mujahidabdi5070@gmail.com>
2024-01-10 17:47:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,77 @@
 function isPalindrome(word) {
   // Write your algorithm here
+    
+      if (word.length === 0 || word.toUpperCase() === word){
+        return "string cannot be empty and write in lowwercase."
+    } 
+    
+    if(typeof word !== "string"){
+        return "only strings allowed"
+    }
+    
+    
+    let reversed_word = ""
+    
+    for(let i = word.length - 1; i >= 0; i--){
+     reversed_word += word[i]; 
+    }
+    
+    if (reversed_word === word ){
+       return true
+    } else {
+     return false
+    }
+    
 }
+    
+     let result = isPalindrome("racecar")
+     console.log(result);
+
+
+
+
+
 
 /* 
   Add your pseudocode here
+  ---------------------------------------------------------------------
+
+  Input the word to be checked
+  if input is empty && lowercase
+     return "string cannot be empty and write in lowwercase."
+        
+        if type of word != string, return "only strings allowed"
+
+      initialize an empty string
+
+      iterate through each letter in the word in reversed mode
+        
+      if origin word === reversed word, return true 
+
+      else
+         return  false
 */
+
+
+
+
 
 /*
   Add written explanation of your solution here
+  ----------------------------------------------------------------------
+
+
+  The function gets an input of a word and checks wether the it is a string and also lower case.
+
+  the function proceeds and initializes an empty string, it then iterates through each letter in reverse format to check 
+
+  if it is equal to the origin string. if so, return true and false if not.
+
+
+
+
+
+
 */
 
 // You can run `node index.js` to view these console logs
